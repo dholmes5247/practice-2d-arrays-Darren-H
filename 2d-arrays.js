92394106a3e47feda9c24bb61c330af26274a237
Darren-Holmes <dholmes5247@hotmail.com>
2025-03-17 11:55:00 -0500

    Updated homework to include labels on my console.logs

--- 2d-arrays.js
+++ 2d-arrays.js
@@ -3,18 +3,18 @@ let allScores = [ // 3 test scores for 3 students
     [88, 99, 91],
     [100, 98,96]
 ];
-console.log(allScores); // Viewing data throughput
+console.log('Student Scores: ', allScores); // Viewing data throughput
 
 let theatreSeating = [  // seating assignments 3 rows of 4 seats
     ['A1', 'A2', 'A3', 'A4'],
     ['B1', 'B2', 'B3', 'B4'],
     ['C1', 'C2', 'C3', 'C4'],
 ];
-console.log(theatreSeating); // Viewing data throughput
+console.log('Seating Chart:', theatreSeating); // Viewing data throughput
 
 let contactsList = [ // each inner is 3 pieces of contact info per person]
     ['Darren', 'dholmes@mail.com', 3142256533],
     ['Stacy', 'stacy@mail.com', 5555555566],
 ];
-console.log(contactsList); // Viewing data throughput
+console.log('Contact Info', contactsList); // Viewing data throughput
 
